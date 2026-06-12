409e447ff88bf66815f67b674be78ce7274682d2
Vamsiayiluri <vamsi.ayiluri@cognine.com>
2026-06-12 12:06:11 +0530

    updated changes for send email issue

--- ipl-auction-tracker-backend/src/utils/emailService.js
+++ ipl-auction-tracker-backend/src/utils/emailService.js
@@ -4,7 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
